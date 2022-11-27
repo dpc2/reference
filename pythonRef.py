1c2cf81742bb2ade103bc4ab288914638777cbb8
dpc2 <dpculler91@gmail.com>
2022-11-26 19:33:27 -0700

    Python containers

--- pythonRef.py
+++ pythonRef.py
@@ -3,10 +3,20 @@
 #------------------------------------------------------------------#
 
 
-#------------------------------#
+#--------------------------#
 #	Virtual Env
-#------------------------------#
+#--------------------------#
 
 # Creat new venv
 python3 -m venv ~/code/python/venv/myNewVenv
 pip install flask
+
+
+#---------------------------#
+#	Containers
+#---------------------------#
+
+myList = ["My list", "iterable", "mutable"]
+myTuple = ("this is my immutable tuple",)
+myDict = {"myDict": "Awesome"}
+print(myDict["myDict"])
