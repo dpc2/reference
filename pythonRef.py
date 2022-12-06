c8edb69d724392205bf0694dd375c787aeaa7c1d
dpc2 <dpculler91@gmail.com>
2022-12-05 22:48:08 -0700

    New Python references

--- pythonRef.py
+++ pythonRef.py
@@ -20,3 +20,55 @@ myList = ["My list", "iterable", "mutable"]
 myTuple = ("this is my immutable tuple",)
 myDict = {"myDict": "Awesome"}
 print(myDict["myDict"])
+
+#---------------------------#
+#     Methods for Lists     #
+#---------------------------#
+
+myList.append(item)
+myList.insert(i,item)
+myList.pop()
+myList.pop(i)
+myList.sort()
+myList.reverse()
+del myList[i]
+myList.index(item)
+myList.count(item)
+myList.remove(item)
+
+#--------------------------#
+#    Methods for Strings   #
+#--------------------------#
+
+myString.center(w)
+myString.count(item)
+myString.ljust(w)
+myString.lower()
+myString.rjust(w)
+myString.find(item)
+myString.split(s_char)
+
+#-------------------------#
+#   Operations on a Set   #
+#-------------------------#
+
+x.in(set)
+len(set)
+set1 | set2
+set1 & set2
+set1 - set2
+set1 <= set2
+
+#------------------------#
+#    Methods for Sets
+#------------------------#
+
+set1.union(set2)
+set1.intersection(set2)
+set1.difference(set2)
+set1.issubset(set2)
+
+set.add(item)
+set.remote(item)
+set.pop()
+set.clear()
