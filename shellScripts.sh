f853cca2199ae360ea1bca063fc501a8872b4afa
dpc2 <dpculler91@gmail.com>
2022-12-07 15:21:25 -0700

    Added new nmap command

--- shellScripts.sh
+++ shellScripts.sh
@@ -9,6 +9,9 @@ ip a
 
 # Scan local network
 nmap 10.0.0.178/24
+# Scan network, only show devices with open port 5555
+nmap -p5555 --open 10.74.86.*
+
 
 # UFW commands
 sudo ufw allow from 10.0.0.129
@@ -16,10 +19,14 @@ sudo ufw allow from 10.0.0.129
 # List all soundcards and digital audio devices
 aplay -l
 
+
+
 # ffmpeg - convert mp3 to wav
 ffmpeg -i dontStop.mp3 -acodec pcm_f32le -ar 44100 dontStop.wav
 # convert wav to mp3
 ffmpeg -i reverseTest.wav -vn -ar 44100 -ac 2 -b:a 192k reverseTest.mp3
+
+
 
 # git things
 # Unstage all files from commit
