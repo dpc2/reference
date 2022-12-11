aa9ecf9c06cb13742f953cd116dac7427e84bd4e
dpc2 <dpculler91@gmail.com>
2022-12-10 21:39:50 -0700

    Modified shellScripts reference file

--- shellScripts.sh
+++ shellScripts.sh
@@ -1,16 +1,20 @@
 # Play bell sound
 printf '\a'
 
+
 # Create desktop icon?
 sudo gnome-desktop-item-edit /usr/share/applications/ --create-new
+
 
 # Read mail spool file
 sudo less /var/mail/$(whoami)
 
+
 # Determine IP address
 ip a
 
-# Scan local network
+
+# Nmap commands
 nmap 10.0.0.178/24
 # Scan network, only show devices with open port 5555
 nmap -p5555 --open 10.74.86.*
@@ -19,16 +23,15 @@ nmap -p5555 --open 10.74.86.*
 # UFW commands
 sudo ufw allow from 10.0.0.129
 
+
 # List all soundcards and digital audio devices
 aplay -l
-
 
 
 # ffmpeg - convert mp3 to wav
 ffmpeg -i dontStop.mp3 -acodec pcm_f32le -ar 44100 dontStop.wav
 # convert wav to mp3
 ffmpeg -i reverseTest.wav -vn -ar 44100 -ac 2 -b:a 192k reverseTest.mp3
-
 
 
 # git things
